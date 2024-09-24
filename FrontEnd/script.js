10db63a97e3ef5aeecd77916701df11c94daaeb9
Gaëtan <eag53@hotmail.fr>
2024-09-24 23:17:05 +0400

    Suppression projets du HTML + JS pour récupération

---
+++ FrontEnd/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Fonction pour afficher les projets sur la page d'accueil
+    function displayProjects() {
+        const gallery = document.querySelector(".gallery");
+        fetch('http://localhost:5678/api/works', {
+            method: 'GET',
+        })    
+        .then(response => response.json())
+        .then(data => {     
+            for (let i = 0; i < data.length; i++) {
+                const project = data[i];
+            
+                // Crée l'affichage des projets
+                const projectFigure = document.createElement("figure");
+                projectFigure.id = project.id;
+                const projectImage = document.createElement("img");
+                projectImage.src = project.imageUrl;
+                projectImage.alt = project.title;
+                projectFigure.appendChild(projectImage);
+                const projectTitle = document.createElement("figcaption");
+                projectTitle.textContent = project.title;
+                projectFigure.appendChild(projectTitle);
+                gallery.appendChild(projectFigure);
+                }
+            })
+        .catch(error => {
+            console.error("Erreur lors de la récupération des projets", error);
+        })
+    }
+
+    displayProjects();
+})
