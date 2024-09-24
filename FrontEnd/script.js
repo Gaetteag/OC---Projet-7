2228444a5657c4b2f3abdc4120af3ad4a4eaf785
Gaëtan <eag53@hotmail.fr>
2024-09-25 00:41:30 +0400

    Ajout filtre Tous en HTML + création autres via JS

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -30,5 +30,34 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     displayProjects();
+    
+    // Fonction pour générer les filtres de catégorie sur la page d'accueil (hors "Tous")
+    function categoryFilters() {
+        fetch('http://localhost:5678/api/categories')
+        .then(response => response.json())
+        .then(categories => { 
+            const uniqueCategoryFilter = new Set();   
 
+            for (let i = 0; i < categories.length; i++) {
+                const category = categories[i];
+
+                if (!uniqueCategoryFilter.has(category.name)) {
+                    uniqueCategoryFilter.add(category.name);
+
+                const categoryFilter = document.createElement("button");
+                categoryFilter.textContent = category.name;
+                categoryFilter.type = "submit";
+                categoryFilter.className = "filters";
+                categoryFilter.id = `filter${category.id}`;
+                const filtersButtons = document.querySelector(".category-filters");
+                filtersButtons.appendChild(categoryFilter);
+                }
+            }
+        }) 
+        .catch(error => {
+            console.error("Erreur lors de la récupération des catégories", error);
+        });
+    }
+    
+    categoryFilters();
 })
