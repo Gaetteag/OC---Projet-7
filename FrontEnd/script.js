6d13bed18252bab7a93d34b98d484b201271a6dc
Gaëtan <eag53@hotmail.fr>
2024-10-02 00:00:01 +0400

    Création page de connexion

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    // Variables
+    const projectsUrl = 'http://localhost:5678/api/works';
+    const categoriesUrl = 'http://localhost:5678/api/categories';
+
     // Fonction pour afficher les projets sur la page d'accueil
     function displayProjects() {
         const gallery = document.querySelector(".gallery");
-        fetch('http://localhost:5678/api/works', {
-            method: 'GET',
-        })    
+        gallery.innerHTML = "";
+        fetch(projectsUrl)    
         .then(response => response.json())
         .then(works => {     
             for (let i = 0; i < works.length; i++) {
@@ -33,26 +36,38 @@ document.addEventListener("DOMContentLoaded", function () {
     
     // Fonction pour générer les filtres de catégorie sur la page d'accueil (hors "Tous")
     function categoryFilters() {
-        fetch('http://localhost:5678/api/categories')
+        fetch(categoriesUrl)
         .then(response => response.json())
         .then(categories => { 
             const uniqueCategoryFilter = new Set();   
-
+            
             for (let i = 0; i < categories.length; i++) {
                 const category = categories[i];
-
+                
                 if (!uniqueCategoryFilter.has(category.name)) {
                     uniqueCategoryFilter.add(category.name);
+                    
+                    const categoryFilterButton = document.createElement("button");
+                    categoryFilterButton.textContent = category.name;
+                    categoryFilterButton.type = "submit";
+                    categoryFilterButton.className = "filters";
+                    categoryFilterButton.id = `filter${category.id}`;
+                    const categoryFilters = document.querySelector(".category-filters");
+                    categoryFilters.appendChild(categoryFilterButton);
 
-                const categoryFilterButton = document.createElement("button");
-                categoryFilterButton.textContent = category.name;
-                categoryFilterButton.type = "submit";
-                categoryFilterButton.className = "filters";
-                categoryFilterButton.id = `filter${category.id}`;
-                const categoryFilters = document.querySelector(".category-filters");
-                categoryFilters.appendChild(categoryFilterButton);
+                    // Ajoute un événement au bouton de chaque catégorie créés via l'API
+                    categoryFilterButton.addEventListener('click', () => {
+                        activateFilter(categoryFilterButton);
+                        filterProjectsByCategory(category.name);
+                    });
                 }
             }
+            // Ajoute un événement au bouton "Tous"
+            const filterAll = document.getElementById("filterAll");
+            filterAll.addEventListener('click', () => {
+                activateFilter(filterAll);
+                displayProjects()
+            });
         }) 
         .catch(error => {
             console.error("Erreur lors de la récupération des catégories", error);
@@ -60,4 +75,27 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     
     categoryFilters();
-})
+    
+    // Fonction pour filtrer les projets selon la catégorie sélectionnée
+    function filterProjectsByCategory(categoryName) {
+        fetch(projectsUrl)
+        .then(response => response.json())
+        .then(works => {
+            const filteredProjects = works.filter(work => work.category.name === categoryName);
+            displayProjects(filteredProjects);
+        })
+        .catch(error => console.error("Erreur lors du filtrage des projets", error));
+    }
+
+    // Fonction pour mettre en surbrillance le filtre actif
+    function activateFilter(selectedFilter) {
+        const allFilters = document.querySelectorAll(".category-filters .filters");
+        for (let i = 0; i < allFilters.length; i++) {
+            if (allFilters[i] === selectedFilter) {
+                allFilters[i].classList.add("filter_selected");
+            } else {
+                allFilters[i].classList.remove("filter_selected");
+            }
+        }
+    }
+});
