d6c56e9e267a327eb7b550a156839b7640ebc782
Gaëtan <eag53@hotmail.fr>
2024-10-08 10:09:56 +0200

    Ajout bannière et bouton modifier

--- FrontEnd/editmode.js
+++ FrontEnd/editmode.js
@@ -1,6 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const token = localStorage.getItem('token');
+    const header = document.querySelector('header');
+    const portfolioSection = document.querySelector('#portfolio');
+    const filtersButton = document.getElementById('category-filters');
+    
+    if (token) {
+        // Afficher les éléments en mode édition si l'utilisateur est connecté
+        transformLoginToLogout();
+
+        // Créer la bannière du mode édition
+        const editModeBanner = document.createElement('div');
+        editModeBanner.classList.add('edit-mode-banner');
+        editModeBanner.id = 'edit-mode-banner';
+
+        const iconBanner = document.createElement('i');
+        iconBanner.classList.add('fa-regular', 'fa-pen-to-square');
+        editModeBanner.appendChild(iconBanner);
+
+        const textBanner = document.createElement('span');
+        textBanner.textContent = 'Mode édition';
+        editModeBanner.appendChild(textBanner);
+    
+        header.insertBefore(editModeBanner, header.firstChild);
+
+        // Créer le bouton modifier du mode édition
+        const editModeButton = document.createElement('div');
+        editModeButton.classList.add('edit-mode-project');
+        editModeButton.id = 'edit-mode-project';
+
+        const modifyLink = document.createElement('a');
+        modifyLink.href = '#modal-section';
+        modifyLink.id = 'modal-modify-button';
+
+        const iconModify = document.createElement('i');
+        iconModify.classList.add('fa-regular', 'fa-pen-to-square');
+        modifyLink.appendChild(iconModify);
+
+        const textModify = document.createElement('span');
+        textModify.textContent = 'modifier';
+        modifyLink.appendChild(textModify);
+
+        editModeButton.appendChild(modifyLink);
+
+        portfolioSection.insertBefore(editModeButton, portfolioSection.children[1]);
+
+        // Retirer les boutons de filtre de l'affichage
+        filtersButton.classList.add('remove-filters-button')
+    }
 
     function transformLoginToLogout() {
         const authLink = document.getElementById('auth-link');
@@ -13,7 +60,5 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     }
-
-    transformLoginToLogout();
 
 });
